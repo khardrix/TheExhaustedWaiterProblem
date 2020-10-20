594e995c7a29311759d3b9953f4378d5703d6eb4
khardrix <carmenandryanhuffman@yahoo.com>
2020-10-20 01:30:43 -0400

    seventh commit

--- src/Customer.java
+++ src/Customer.java
@@ -1,36 +1,24 @@
-// IMPORTS of Java needed tools and plug-ins
 import java.lang.Thread;
 import java.util.concurrent.Semaphore;
-
+import java.lang.ThreadGroup;
 
 public class Customer extends Thread {
-
-
-    private volatile Semaphore Door = new Semaphore(15, true);
-    private volatile Semaphore Nap = new Semaphore(0, true);
-    private volatile Semaphore Servicing = new Semaphore(0, true);
-    private volatile ThreadGroup rushhour = new ThreadGroup("rushhour group");
-    private volatile ThreadGroup showtime = new ThreadGroup("showtime group");
-
-
-    private static int custCount = 0;
-
+    private volatile Semaphore Door;
+    private volatile Semaphore Nap;
+    private volatile Semaphore Servicing;
 
     public Customer (Semaphore Nap, Semaphore Servicing, Semaphore Door, ThreadGroup threadGroup) {
+        super(threadGroup, null, null);
         this.Nap = Nap;
         this.Servicing = Servicing;
         this.Door = Door;
-        threadGroup = threadGroup;
     }
 
-
-
-    @Override
+    // @Override
     public void run() {
         Nap.release();
-
-        Servicing.tryAcquire();
-
+        // Servicing.acquire();
         Door.release();
     }
 }
+
